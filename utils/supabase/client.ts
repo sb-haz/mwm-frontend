eb5b4edfc2609a1606fdc02701deb9ed9b117f9a
sb-haz <sb_haz@icloud.com>
2024-07-11 21:13:50 +0100

    first commit

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -4,4 +4,4 @@ export const createClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+  ); 
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -75,4 +75,4 @@ export const updateSession = async (request: NextRequest) => {
       },
     });
   }
-};
+}; 
