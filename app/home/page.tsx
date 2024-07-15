9474180e6dbae50a1a269058b2439d0756fcc3d6
sb-haz <sb_haz@icloud.com>
2024-07-15 21:37:50 +0100

    style: Update image sizes and button text size in home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -20,14 +20,15 @@ export default async function Home() {
                 <div className="live-container">
 
                     <div className="center-items">
-                        <img src="home-couple.png" className="image-full-width home-couple-image margin-top-15" width={511} height={414} />
+                        <img src="home-couple.png" className="image-full-width home-couple-image margin-top-15" width={625} height={373} />
+                        <div className="break"></div>
                         <img src="mwm-logo.png" className="image-full-width home-logo-image" width={534} height={104} />
                     </div>
 
                     <div className="button-container margin-top-20">
                         <Link href="home">
                             <div className="explore-app-div">
-                                <button className="button pink-button button-large button-text-white text-thick glow-effect">Scan Wedding QR Code</button>
+                                <button className="button pink-button button-large button-text-white text-thick button-text-large glow-effect">Scan Wedding QR Code</button>
                                 <img src="up-arrow.png" className="up-arrow" />
                             </div>
                         </Link>
@@ -36,7 +37,7 @@ export default async function Home() {
                     <div className="button-container margin-top-15">
                         <Link href="marketplace">
                             <div className="explore-app-div">
-                                <button className="button grey-button button-large button-min-xlarge">Explore the App</button>
+                                <button className="button grey-button button-large button-min-xlarge button-text-large">Explore the App</button>
                                 <p className="text-xs text-extra-light-grey explore-app-text">Marketplace, planning tools & more</p>
                                 <img src="up-arrow-flipped.png" className="up-arrow-second" />
                             </div>
