6d1f1eb0c6ebfedef4eb962f00427246ba04b56d
sb-haz <sb_haz@icloud.com>
2024-07-20 16:29:53 +0100

    style: Update dashboard.css and page.tsx for couple page dashboard

---
+++ app/couple-page-dashboard/page.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import Footer from '@/components/Footer';
+import Link from 'next/link';
+
+import '../styles/styles.css';
+import '../styles/live.css';
+import '../styles/search.css';
+import '../styles/dashboard.css';
+
+export default async function CouplePageDashboard() {
+
+    return (
+        <div>
+            {/* Top Art */}
+            <img src="flower-art-top.png" className="image-full-width" width={966} height={301} />
+
+            <div className="container reduce-container-margin">
+                {/* Image Above Boxes */}
+                <div className="dashboard-image-above-boxes grow-effect">
+                    <img src="couples-name.png" alt="Image Above Boxes" width={223} height={42} />
+                </div>
+
+                {/* Dashboard Boxes Container */}
+                <div className="dashboard-box-container">
+                    {/* Dashboard Box 1 */}
+                    <div className="dashboard-box live-dashboard box-drop-shadow-03">
+                        <h2 className="heading-medium">Send Photos</h2>
+                        <img className="dashboard-icon" src="dash-1.png" alt="" />
+                    </div>
+
+                    {/* Dashboard Box 2 */}
+                    <div className="dashboard-box live-dashboard box-drop-shadow-03">
+                        <h2 className="heading-medium">Send Videos</h2>
+                        <img className="dashboard-icon" src="dash-2.png" alt="" />
+                    </div>
+
+                    {/* Dashboard Box 3 */}
+                    <div className="dashboard-box live-dashboard box-drop-shadow-03">
+                        <h2 className="heading-medium">Gifting</h2>
+                        <img className="dashboard-icon" src="dash-3.png" alt="" />
+                    </div>
+
+                    {/* Dashboard Box 4 */}
+                    <div className="dashboard-box live-dashboard box-drop-shadow-03">
+                        <h2 className="heading-medium">Quiz</h2>
+                        <img className="dashboard-icon" src="dash-4.png" alt="" />
+                    </div>
+
+                    {/* Dashboard Box 5 */}
+                    <div className="dashboard-box live-dashboard box-drop-shadow-03">
+                        <h2 className="heading-medium">Event</h2>
+                        <img className="dashboard-icon" src="dash-5.png" alt="" />
+                    </div>
+
+                    {/* Dashboard Box 6 */}
+                    <div className="dashboard-box live-dashboard box-drop-shadow-03">
+                        <h2 className="heading-medium">Chat</h2>
+                        <img className="dashboard-icon" src="dash-6.png" alt="" />
+                    </div>
+                </div>
+
+                {/* Button Container */}
+                <div className="button-container margin-top-50 margin-bottom-15">
+                    <Link href="home">
+                        <div className="explore-app-div">
+                            <button className="button grey-button button-medium button-text-medium">Exit Wedding</button>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+
+            <div className="container-bottom-s"></div>
+        </div>
+    );
+}
