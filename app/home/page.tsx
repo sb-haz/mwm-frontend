5bec540eb7527bacb9139e057ef4275b046fecdd
sb-haz <sb_haz@icloud.com>
2024-07-20 20:55:15 +0100

    Update image dimensions and sizes for better mobile display

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Footer from '@/components/Footer';
 import Link from 'next/link';
 
@@ -9,45 +9,38 @@ import '../styles/services.css';
 import '../styles/home.css';
 import '../styles/live.css';
 
-
 export default async function Home() {
-
     return (
-
         <div>
-            <img src="flower-art-top-2.png" className="image-full-width top-banner-image" width={375} height={117} />
+            <img src="/flower-art-top-2.png" className="image-full-width top-banner-image" alt="Flower Art" width={375} height={117} />
             <div className="container">
                 <div className="live-container">
-
                     <div className="center-items">
-                        <img src="home-couple.png" className="image-full-width home-couple-image margin-top-15" width={625} height={373} />
+                        <img src="/home-couple.png" className="image-full-width home-couple-image margin-top-15" alt="Home Couple" width={625} height={373} />
                         <div className="break"></div>
-                        <img src="mwm-logo.png" className="image-full-width home-logo-image" width={534} height={104} />
+                        <img src="/mwm-logo.png" className="image-full-width home-logo-image" alt="MWM Logo" width={534} height={104} />
                     </div>
-
                     <div className="button-container margin-top-20">
                         <Link href="scan-qr-code">
                             <div className="explore-app-div">
                                 <button className="button pink-button button-large button-text-white text-thick button-text-large glow-effect">Scan Wedding QR Code</button>
-                                <img src="up-arrow.png" className="up-arrow" />
+                                <img src="/up-arrow.png" className="up-arrow" alt="Up Arrow" />
                             </div>
                         </Link>
                     </div>
-
                     <div className="button-container margin-top-15">
                         <Link href="marketplace">
                             <div className="explore-app-div">
                                 <button className="button grey-button button-large button-min-xlarge button-text-large">Explore the App</button>
                                 <p className="text-xs text-extra-light-grey explore-app-text">Marketplace, planning tools & more</p>
-                                <img src="up-arrow-flipped.png" className="up-arrow-second" />
+                                <img src="/up-arrow-flipped.png" className="up-arrow-second" alt="Up Arrow Flipped" />
                             </div>
                         </Link>
                     </div>
                 </div>
-
                 <div className="container-bottom"></div>
                 <Footer activePage="home" />
-            </div >
+            </div>
         </div>
-    )
+    );
 }
