215c0ed858526c676a7481cbf1d84c6330d77009
sb-haz <sb_haz@icloud.com>
2024-08-02 00:13:44 +0100

    Update protected page layout and navigation styling

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -17,14 +17,14 @@ export default async function ProtectedPage() {
   }
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="w-full">
-        <div className="py-6 font-bold bg-purple-950 text-center">
+    <div className="">
+      <div className="">
+        <div className="">
           This is a protected page that you can only see as an authenticated
           user
         </div>
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+        <nav className="">
+          <div className="">
             <AuthButton />
           </div>
         </nav>
